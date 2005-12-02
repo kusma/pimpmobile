ce1c2beb6ff6f30ff09cf4faad2d403f354c4bbb
Erik Faye-Lund <kusmabite@gmail.com>
2005-12-02 17:53:03 +0000

    some more fields loaded
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@45 3d5ecaf0-f903-0410-b953-c2c1a4d75763

--- converter_s3m.cpp
+++ converter_s3m.cpp
@@ -7,13 +7,42 @@
 
 module_t *load_module_s3m(FILE *fp)
 {
-	fseek(fp, 0x2C, SEEK_SET);
 	
+	/* check file-signature */
 	char sig[4];
+	fseek(fp, 0x2C, SEEK_SET);
 	fread(sig, 1, 4, fp);
 	if (memcmp(sig, "SCRM", 4) != 0) return NULL;
 	
+	/* check a second signature. */
+	unsigned char sig2;
+	fseek(fp, 0x1C, SEEK_SET);
+	fread(&sig2, 1, 1, fp);
+	if (sig2 != 0x1A) return NULL;
+	
+	/* check that the type is correct. just another sanity-check. */
+	unsigned char type;
+	fread(&type, 1, 1, fp);
+	if (type != 16) return NULL;
+	
 	printf("huston, we have s3m.\n");
+	
+	/* check that the type is correct. just another sanity-check. */
+	char name[28 + 1];
+	fseek(fp, 0x0, SEEK_SET);
+	fread(&name, 1, 28, fp);
+	name[28] = '\0';
+	printf("it's called \"%s\"\n", name);
+	
+	unsigned short ordnum, insnum, patnum, flags;
+	fseek(fp, 0x20, SEEK_SET);
+	fread(&ordnum, 1, 2, fp);
+	fread(&insnum, 1, 2, fp);
+	fread(&patnum, 1, 2, fp);
+	fread(&flags,  1, 2, fp);
+	
+	printf("orders: %d\ninstruments: %d\npatterns: %d\nflags: %x\n", ordnum, insnum, patnum, flags);
+
 	
 	module_t *mod = (module_t *)malloc(sizeof(module_t));
 	if (mod == 0)
