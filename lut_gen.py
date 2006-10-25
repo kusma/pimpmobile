e9d8cc6351e3d369e6fb7e6436f601076f22cc5f
Erik Faye-Lund <kusmabite@gmail.com>
2006-10-25 00:36:57 +0000

    a bit neater python (not very readable, though ;)
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@168 3d5ecaf0-f903-0410-b953-c2c1a4d75763

--- lut_gen.py
+++ lut_gen.py
@@ -11,43 +11,45 @@ AMIGA_DELTA_LUT_SIZE = 1 << AMIGA_DELTA_LUT_LOG_SIZE
 AMIGA_DELTA_LUT_FRAC_BITS = 15 - AMIGA_DELTA_LUT_LOG_SIZE
 
 def print_lut(lut):
-    print 'const u16 linear_delta_lut[768] =\n{'
-    for e in lut:
-        print '%d,' % (e),
-    print '\n};\n'
+	print 'const u16 linear_delta_lut[768] =\n{'
+	for e in lut:
+		print '%d,' % (e),
+	print '\n};\n'
 
 def print_lut_to_file(file, lutname, lut):
-    file.write('const u16 %s[%d] =\n{\n\t' % (lutname, len(lut)))
-    line_start = file.tell()
-    for e in lut:
-        file.write('%d, ' % e)
-        if file.tell() > (line_start + 80):
-            file.write('\n\t')
-            line_start = file.tell()    
-    file.write('\n};\n\n')
+#	max_elem = reduce(max, lut)
+#	min_elem = reduce(min, lut)
+#	if (min_elem < 0):         print 'need signed'
+#	if (max_elem > (1 << 16)): print 'need more than 16 bit'
+#	print 'max: %d, min %d' % (max_elem, min_elem)
+
+	file.write('const u16 %s[%d] =\n{\n\t' % (lutname, len(lut)))
+	line_start = file.tell()
+	for e in lut:
+		file.write('%d, ' % e)
+		if file.tell() > (line_start + 80):
+			file.write('\n\t')
+			line_start = file.tell()    
+	file.write('\n};\n\n')
 
 def gen_linear_delta_lut():
-    lut = []
-    for i in range(0, 12 * 64):
-        val = int(((2.0 ** (i / 768.0)) * 8363.0 / (1 << 8)) * float(1 << 9) + 0.5)
-        lut.append(val)
-    return lut
+	return list((int(((2.0 ** (i / 768.0)) * 8363.0 / (1 << 8)) * float(1 << 9) + 0.5)) for i in range(0, 12 * 64))
 
 def gen_amiga_delta_lut():
-    lut = []
-    for i in range(0, (AMIGA_DELTA_LUT_SIZE / 2) + 1):
-        p = i + (AMIGA_DELTA_LUT_SIZE / 2);
-        val = int(((8363 * 1712) / float((p * 32768) / AMIGA_DELTA_LUT_SIZE)) * (1 << 6) + 0.5);
-        lut.append(val)
-    return lut
+	return list((int(((8363 * 1712) / float(((i + (AMIGA_DELTA_LUT_SIZE / 2)) * 32768) / AMIGA_DELTA_LUT_SIZE)) * (1 << 6) + 0.5)) for i in range(0, (AMIGA_DELTA_LUT_SIZE / 2) + 1))
 
-linear_delta_lut = gen_linear_delta_lut()
-f = open('src/linear_delta_lut.h', 'w')
-print_lut_to_file(f, 'linear_delta_lut', linear_delta_lut)
-f.close()
+def dump_linear_lut(filename):
+	linear_delta_lut = gen_linear_delta_lut()
+	f = open(filename, 'w')
+	print_lut_to_file(f, 'linear_delta_lut', linear_delta_lut)
+	f.close()
 
-amiga_delta_lut = gen_amiga_delta_lut()
-f = open('src/amiga_delta_lut.h', 'w')
-f.write('#define AMIGA_DELTA_LUT_LOG_SIZE %d\n' % (AMIGA_DELTA_LUT_LOG_SIZE))
-print_lut_to_file(f, 'amiga_delta_lut', amiga_delta_lut)
-f.close()
+def dump_amiga_lut(filename):
+	amiga_delta_lut = gen_amiga_delta_lut()
+	f = open(filename, 'w')
+	f.write('#define AMIGA_DELTA_LUT_LOG_SIZE %d\n' % (AMIGA_DELTA_LUT_LOG_SIZE))
+	print_lut_to_file(f, 'amiga_delta_lut', amiga_delta_lut)
+	f.close()
+
+dump_linear_lut('src/linear_delta_lut.h')
+dump_amiga_lut('src/amiga_delta_lut.h')
