71d9782d8d7a76cd56f8e03d2ee071d146c3ce23
Erik Faye-Lund <kusmabite@gmail.com>
2006-11-28 11:13:50 +0000

    updated makefs-scripts a tad, now both support multiple files
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@193 3d5ecaf0-f903-0410-b953-c2c1a4d75763

--- makefs.sh
+++ makefs.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-bin/converter eye.mod
-gbfs data.gbfs sample_bank.bin eye.mod.bin
-copy /B example.bin+data.gbfs example.gba
+# check parameters
+if [ -z "$@" ]
+then
+	echo "USAGE: makefs.bat filename.(mod/xm/s3m)"
+	exit 1
+fi
+
+# convert
+bin/converter $@
+[ $? -ne 0 ] && exit 1
+
+# make filesystem
+gbfs data.gbfs sample_bank.bin `echo $@ | sed -e "s/[^ ]\+/\0.bin/g"`
+
+# append filesystem to rom
+cat bin/example.bin > example.gba
+cat data.gbfs >> example.gba
