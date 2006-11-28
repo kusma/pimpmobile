8ade29b0166959a46bfde7472ebe74c5501ab533
Erik Faye-Lund <kusmabite@gmail.com>
2006-11-28 11:13:03 +0000

    clarified the lutgen code a bit
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@192 3d5ecaf0-f903-0410-b953-c2c1a4d75763

--- lut_gen.py
+++ lut_gen.py
@@ -8,12 +8,6 @@ AMIGA_DELTA_LUT_LOG2_SIZE = 7
 AMIGA_DELTA_LUT_SIZE = 1 << AMIGA_DELTA_LUT_LOG2_SIZE
 AMIGA_DELTA_LUT_FRAC_BITS = 15 - AMIGA_DELTA_LUT_LOG2_SIZE
 
-def print_lut(lut):
-	print 'const u16 linear_delta_lut[768] =\n{'
-	for e in lut:
-		print '%d,' % (e),
-	print '\n};\n'
-
 def print_lut_to_file(file, lutname, lut):
 	max_elem = reduce(max, lut)
 	min_elem = reduce(min, lut)
@@ -28,11 +22,26 @@ def print_lut_to_file(file, lutname, lut):
 			line_start = file.tell()
 	file.write('\n};\n\n')
 
+
+AMIGA_C4_FREQ        = 8363.0
+AMIGA_C4_PERIOD      = 1712
+NOTES_PER_OCTAVE     = 12
+FINETUNES_PER_NOTE   = 64
+FINETUNES_PER_OCTAVE = NOTES_PER_OCTAVE * FINETUNES_PER_NOTE
+
+def linear_delta_entry(note):
+	temp = 2.0 ** (float(note) / FINETUNES_PER_OCTAVE)
+	return int((temp * AMIGA_C4_FREQ) * 2 + 0.5)
+
+def amiga_delta_entry(fine_note):
+	temp = float(((fine_note + (AMIGA_DELTA_LUT_SIZE / 2)) * (1 << 15)) / AMIGA_DELTA_LUT_SIZE)
+	return int(((AMIGA_C4_FREQ * AMIGA_C4_PERIOD) / temp) * (1 << 6) + 0.5)
+
 def gen_linear_delta_lut():
-	return [(int(((2.0 ** (i / 768.0)) * 8363.0 / (1 << 8)) * float(1 << 9) + 0.5)) for i in range(0, 12 * 64)]
+	return [linear_delta_entry(i) for i in range(0, FINETUNES_PER_OCTAVE)]
 
 def gen_amiga_delta_lut():
-	return [(int(((8363 * 1712) / float(((i + (AMIGA_DELTA_LUT_SIZE / 2)) * 32768) / AMIGA_DELTA_LUT_SIZE)) * (1 << 6) + 0.5)) for i in range(0, (AMIGA_DELTA_LUT_SIZE / 2) + 1)]
+	return [amiga_delta_entry(i) for i in range(0, (AMIGA_DELTA_LUT_SIZE / 2) + 1)]
 
 def dump_linear_lut(filename):
 	linear_delta_lut = gen_linear_delta_lut()
