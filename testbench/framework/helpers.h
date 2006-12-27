2821df1524a523b0e6a38e98b445af79323b79e3
Erik Faye-Lund <kusmabite@gmail.com>
2006-12-27 16:58:38 +0000

    added some helper stuff
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@239 3d5ecaf0-f903-0410-b953-c2c1a4d75763

---
+++ testbench/framework/helpers.h
@@ -0,0 +1,4 @@
+void test_int_array(const int *array, const int *reference, int size, const char *file, int line);
+
+#define TEST_INTS_EQUAL(value, expected) TEST((value) == (expected), test_printf("ints not equal, got %d - expected %d (%s:%d)", (int)(value), (int)(expected), __FILE__, __LINE__))
+#define TEST_INT_ARRAYS_EQUAL(array, reference, size) test_int_array(array, reference, size, __FILE__, __LINE__)
