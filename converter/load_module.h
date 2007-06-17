c571b04788af6060578b3a42d1eb135ecd006643
Erik Faye-Lund <kusmabite@gmail.com>
2007-06-17 21:52:18 +0000

    on-target loading works (and we have an example)
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@307 3d5ecaf0-f903-0410-b953-c2c1a4d75763

--- converter/load_module.h
+++ converter/load_module.h
@@ -1,6 +1,10 @@
 #ifndef LOAD_MODULE_H
 #define LOAD_MODULE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h> /* needed for FILE */
 
 struct pimp_module;
@@ -8,5 +12,9 @@ struct pimp_sample_bank;
 
 struct pimp_module *load_module_mod(FILE *fp);
 struct pimp_module *load_module_xm(FILE *fp, struct pimp_sample_bank *sample_bank);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LOAD_MODULE_H */
---
+++ example/example2.cpp
@@ -0,0 +1,126 @@
+/* example.cpp -- Pimpmobile GameBoy Advance example
+ * Copyright (C) 2005-2006 Jørn Nystad and Erik Faye-Lund
+ * For conditions of distribution and use, see copyright notice in LICENSE.TXT
+ */
+
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+#include <gba_timers.h>
+
+#ifndef REG_WAITCNT
+#define REG_WAITCNT (*(vu16*)(REG_BASE + 0x0204))
+#endif
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#include "../include/pimpmobile.h"
+#include "../converter/load_module.h"
+#include "../src/pimp_sample_bank.h"
+#include "gbfs_stdio.h"
+
+int accum = 0;
+void mix()
+{
+	REG_TM2CNT_L = 0;
+	REG_TM2CNT_H = 0;
+	REG_TM2CNT_H = TIMER_START;
+	pimp_vblank();
+	pimp_frame();
+	u32 value = REG_TM2CNT_L;
+	accum += value;
+}
+
+void report()
+{
+	float percent = float(accum * 100) / (1 << 24);
+	iprintf("cpu: %d.%03d%% (%d c/f)\n", int(percent), int(percent * 1000) % 1000, accum / 60);
+	accum = 0;
+}
+
+void timer3()
+{
+	report();
+}
+
+void vblank()
+{
+	mix();
+}
+
+void callback(int type, int data)
+{
+	switch (type)
+	{
+		case PIMP_CALLBACK_SYNC:
+		break;
+		
+		case PIMP_CALLBACK_UNSUPPORTED_EFFECT:
+			iprintf("eff: %X\n", data);
+		break;
+		
+		case PIMP_CALLBACK_UNSUPPORTED_VOLUME_EFFECT:
+			iprintf("vol eff: %X\n", data);
+		break;
+	}
+}
+
+int main()
+{
+//	REG_WAITCNT = 0x46d6; // lets set some cool waitstates...
+	REG_WAITCNT = 0x46da; // lets set some cool waitstates...
+	
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+	consoleInit(0, 4, 0, NULL, 0, 15);
+
+	BG_COLORS[0] = RGB5(0, 0, 0);
+	BG_COLORS[241] = RGB5(31, 31, 31);
+	REG_DISPCNT = MODE_0 | BG0_ON;
+
+	gbfs_init(1);
+	
+	pimp_sample_bank sb;
+	FILE *fp = fopen("dxn-oopk.xm", "rb");
+	if (!fp)
+	{
+		fprintf(stderr, "file not found\n");
+		return 1;
+	}
+	pimp_module *mod = load_module_xm(fp, &sb);
+	fclose(fp);
+	fp = NULL;
+	
+	if (NULL == mod)
+	{
+		fprintf(stderr, "failed to load module\n");
+		return 1;
+	}
+
+	pimp_init(mod, sb.data);
+	pimp_set_callback(callback);
+
+	irqSet(IRQ_TIMER3, timer3);
+	irqEnable(IRQ_TIMER3);
+	REG_TM3CNT_L = 0;
+	REG_TM3CNT_H = TIMER_START | TIMER_IRQ | 2;
+	
+	irqSet(IRQ_VBLANK, vblank);
+	irqEnable(IRQ_VBLANK);
+	
+	while (1)
+	{
+		VBlankIntrWait();
+		scanKeys();
+		int keys = keysDown();
+		if (keys & KEY_RIGHT) pimp_set_pos(0, pimp_get_order() + 1);
+		if (keys & KEY_LEFT)  pimp_set_pos(pimp_get_row() + 8, pimp_get_order());
+	}
+	
+	pimp_close();
+	return 0;
+}
