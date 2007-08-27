3a1abed5b26000902cba3bc71f47233942fe6b86
Erik Faye-Lund <kusmabite@gmail.com>
2007-08-27 23:59:59 +0000

    added c++ guard
    git-svn-id: https://pimpmobile.svn.sourceforge.net/svnroot/pimpmobile/trunk@334 3d5ecaf0-f903-0410-b953-c2c1a4d75763

--- src/pimp_sample_bank.h
+++ src/pimp_sample_bank.h
@@ -1,6 +1,10 @@
 #ifndef PIMP_SAMPLE_BANK_H
 #define PIMP_SAMPLE_BANK_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "pimp_types.h"
 #include "pimp_base.h"
 #include "pimp_debug.h"
@@ -23,5 +27,9 @@ static INLINE void *pimp_sample_bank_get_sample_data(const pimp_sample_bank *sam
 
 int   pimp_sample_bank_find_sample_data(const pimp_sample_bank *sample_bank, void *data, pimp_size_t len);
 int   pimp_sample_bank_insert_sample_data(pimp_sample_bank *sample_bank, void *data, pimp_size_t len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* PIMP_SAMPLE_BANK_H */
